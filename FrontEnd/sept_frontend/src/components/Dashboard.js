ebc7b99964cf3faca07fd6dfc0435cd768feb9bd
Darren Lieu s3780893 <s3780893@student.rmit.edu.au>
2020-09-02 18:18:51 +1000

    Setup frontend following the tutorial

---
+++ FrontEnd/sept_frontend/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Person from './Persons/Person'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <Person/>
+            </div>
+        )
+    }
+}
+export default Dashboard;
