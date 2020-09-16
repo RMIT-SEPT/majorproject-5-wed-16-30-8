50af13d3602e0e86474ee7424fc7035b35f6dc8f
Mitch <mitchell@softzone.com.au>
2020-09-16 16:19:08 +1000

    - add person validation method - add more unit testing

--- BackEnd/src/test/java/com/rmit/assignment/demo/model/PersonTest.java
+++ BackEnd/src/test/java/com/rmit/assignment/demo/model/PersonTest.java
@@ -1,19 +1,40 @@
 package com.rmit.assignment.demo.model;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
+    //valid person
+    Person tony = new Person(200, "tony", "Cust", "a New person");
+    //name is too short
+    Person yu = new Person(201,"Yu","","");
+    //has no description
+    Person jack = new Person(202, "Jack","Cust","");
+    //has a space in the name
+    Person luka = new Person(203, "Luka Smith", "Cust", "full name in name field");
+    //has symbols in the name
+    Person symbols = new Person(204, "!*$symbol@%#", "Customer", "has symbols in the name");
 
     @Test
-    public void incompletePerson(){
-        System.out.println("Test 1: Incomplete Person entry");
-        Person person = new Person(200, "tony", "Customer", "a New person");
+    public void getNameTest(){
+        System.out.println("Test 1: get persons name entry");
+        Assertions.assertEquals("tony", tony.getName());
+    }
 
-        Assertions.assertEquals("tony", person.getName());
+    @Test
+    public void isValidPersonTest(){
+        System.out.println("Test 2: the person has valid details");
+        Assertions.assertTrue(tony.isValidPerson());
+    }
 
+    @Test
+    public void isNotValidPersonTest(){
+        System.out.println("Test 3: person has invalid details");
+        Assertions.assertFalse(yu.isValidPerson());
     }
 
 }
