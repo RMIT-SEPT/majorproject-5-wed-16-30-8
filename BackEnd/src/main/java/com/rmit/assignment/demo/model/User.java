5899d4d3c4a587169d2a0ba5cdac9184e793052f
Mitch <mitchell@softzone.com.au>
2020-09-29 18:29:13 +1000

    - modify user

--- BackEnd/src/main/java/com/rmit/assignment/demo/model/User.java
+++ BackEnd/src/main/java/com/rmit/assignment/demo/model/User.java
@@ -2,14 +2,18 @@ package com.rmit.assignment.demo.model;
 
 
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Collection;
 import java.util.Date;
 
 @Entity
 @DiscriminatorValue(value = "USER")
-public class User extends Person {
+public class User extends Person implements UserDetails {
 
 
     public User(String name, String personIdentifier, String password, String ph_Num,String address, Date created_At){
@@ -21,4 +25,33 @@ public class User extends Person {
     }
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
